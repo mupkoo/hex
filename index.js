361b746971e1103aba15e60cd592a406bd632ca8
Mirko Akov <mupkoo@gmail.com>
2018-12-10 19:43:36 +0200

    Call ember-basic-dropdown contentFor hook as well

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ module.exports = {
   name: require('./package').name,
 
   contentFor(type, config) {
+    let emberBasicDropdown = this.addons.find((a) => a.name === 'ember-basic-dropdown');
+    let emberBasicDropdownContentFor = emberBasicDropdown.contentFor(type, config);
+
     if (config.environment !== 'test' && type === 'body-footer' && !config._hexContentForInvoked) {
       config._hexContentForInvoked = true;
 
       return `
+        ${emberBasicDropdownContentFor}
         <div id="hex-modal-parent"></div>
       `;
     }
+
+    return emberBasicDropdownContentFor;
   }
 };
