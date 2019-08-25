8c154d3a66fb4005f31b4c4e4d20e4ef57510be3
Mirko Akov <mupkoo@gmail.com>
2019-08-26 00:37:28 +0300

    Replace the TextArea component with TextAreaField

--- tests/dummy/app/docs/components/text-area-field/route.js
+++ tests/dummy/app/docs/components/text-area-field/route.js
@@ -1,6 +1,6 @@
 import Route from '@ember/routing/route';
 
-export default class TextAreaRoute extends Route {
+export default class TextAreaFieldRoute extends Route {
   model() {
     return 'The universe is almost 14 billion years old!';
   }
