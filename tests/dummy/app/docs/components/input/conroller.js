2ada112bd0f5e4469f808a6abc57b5e08724960f
Mirko Akov <mupkoo@gmail.com>
2019-08-25 18:22:05 +0300

    Remove the Label and Input components
    Since Ember enabled angle bracket invocation for the built in components, they cannot be overwritten

--- tests/dummy/app/docs/components/input/conroller.js
+++
@@ -1,6 +0,0 @@
-import Controller from '@ember/controller';
-import { alias } from '@ember/object/computed';
-
-export default class InputController extends Controller {
-  @alias('model') value;
-}
