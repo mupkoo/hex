67fd8e787554da8c7274098341b871ee806231d7
Mirko Akov <mupkoo@gmail.com>
2019-08-25 18:31:36 +0300

    Add InputField component

---
+++ tests/dummy/app/docs/components/input-field/template.md
@@ -0,0 +1,29 @@
+# Input Field
+
+Input field component
+
+One of the ways to use the component, is to provide `@value` property. Doing that
+the property will get updated in the input event. If you want to manually handle 
+this, you can provide `@onChange` handler which will return the value of the input.
+This is more or less equal to `oninput={{action (mut this.value) value="target.value"}}`.
+
+{{#docs-demo as |demo|}}
+  {{#demo.example name="input-field-example.hbs"}}
+    <InputField 
+      @label="Secret" 
+      @value={{this.model}}
+      type="text"
+      {{on "focus" (fn (mut this.currentEvent) "'on focus' called")}}
+      {{on "blur" (fn (mut this.currentEvent) "'on blur' called")}}
+      class="custom-class"
+    />
+
+    <div class="docs-debug">
+      Debug: {{this.currentEvent}}<br>
+      Current value: {{this.model}}
+    </div>
+  {{/demo.example}}
+
+  {{demo.snippet "input-field-example.hbs"}}
+  {{demo.snippet "input-field-example.js"}}
+{{/docs-demo}}
---
+++ tests/integration/components/input-field-test.js
@@ -0,0 +1,85 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { fillIn, render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration: InputField', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it render a label if the @label is passed', async function (assert) {
+    await render(hbs`<InputField @label="Awesome input" />`);
+
+    assert.dom('label').hasText('Awesome input');
+  });
+
+  test('it does not render label if one is not provided', async function (assert) {
+    await render(hbs`<InputField />`);
+
+    assert.dom('label').doesNotExist();
+  });
+
+  test('it sets the for and id attribute of the label and the input', async function (assert) {
+    await render(hbs`<InputField @label="Awesome" />`);
+
+    let id = this.element.querySelector('input').id;
+
+    assert.ok(id);
+    assert.dom('label').hasAttribute('for', id);
+  });
+
+  test('it mutates the passed @value', async function (assert) {
+    this.value = 'First';
+
+    await render(hbs`<InputField @value={{this.value}} />`);
+
+    await fillIn('input', 'Awesome');
+
+    assert.equal(this.value, 'Awesome');
+  });
+
+  test('it triggers the passed @onChange action on input', async function (assert) {
+    this.onChange = (value) => assert.step(value);
+
+    await render(hbs`<InputField @onChange={{this.onChange}} />`);
+
+    await fillIn('input', 'Most gracious');
+
+    assert.verifySteps(['Most gracious']);
+  });
+
+  test('it does not mutate the @value if @onChange is provided', async function (assert) {
+    this.value = 'OG';
+    this.onChange = (value) => assert.step(value);
+
+    await render(hbs`<InputField @value={{this.value}} @onChange={{this.onChange}} />`);
+
+    await fillIn('input', 'Rip off');
+
+    assert.verifySteps(['Rip off']);
+    assert.equal(this.value, 'OG');
+  });
+
+  test('it passes the attributes to the input', async function (assert) {
+    await render(hbs`<InputField data-test-input type="number" min="1" max="5" required={{true}} />`);
+
+    assert.dom('input').hasAttribute('data-test-input');
+    assert.dom('input').hasAttribute('type', 'number');
+    assert.dom('input').hasAttribute('min', '1');
+    assert.dom('input').hasAttribute('max', '5');
+    assert.dom('input').hasAttribute('required');
+  });
+
+  test('it works using the normal value and a modifier', async function (assert) {
+    this.value = 'initial value';
+    this.onInput = (e) => this.set('value', e.target.value);
+
+    await render(hbs`<InputField value={{this.value}} {{on "input" this.onInput}} />`);
+
+    assert.dom('input').hasValue(this.value);
+
+    await fillIn('input', 'New value');
+
+    assert.dom('input').hasValue('New value');
+    assert.equal(this.value, 'New value');
+  });
+});
