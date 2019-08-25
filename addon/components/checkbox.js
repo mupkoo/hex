9a7487173462093f09d47b594e5dcb983e5eb6e3
Mirko Akov <mupkoo@gmail.com>
2019-08-26 00:37:59 +0300

    Tweak the Checkbox component so it matches the rest field components

--- addon/components/checkbox.js
+++ addon/components/checkbox.js
@@ -1,8 +1,11 @@
 import Component from '@ember/component';
+import { action } from '@ember/object';
 import layout from '../templates/components/checkbox';
+import uuid from '../-private/uuid';
 
 /**
   HTML <input type="checkbox"> wrapper
+
   ```hbs
   <Checkbox />
   ```
@@ -12,4 +15,16 @@ import layout from '../templates/components/checkbox';
 export default class CheckboxComponent extends Component {
   layout = layout;
   tagName = '';
+
+  get uuid() {
+    return 'hex-' + uuid();
+  }
+
+  @action handleChangeEvent(e) {
+    if (this.onChange) {
+      this.onChange(e.target.checked);
+    } else {
+      this.set('checked', e.target.checked);
+    }
+  }
 }
